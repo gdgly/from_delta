991cfcb5269d6c64e45352e34cc7aba9e0306ae8
tiancai <fanxuefeng8891@outlook.com>
2020-01-06 10:57:23 +0800

    1

--- C/linearformat.c
+++ C/linearformat.c
@@ -58,56 +58,57 @@ PMBUS_tData.u32Pin_Mul_128.u32Val
 #define TEMPERATURE_FRC_RESULUTION 		64
 #define TEMPERATURE_FRC_NUM						6
 
-/*
-static uint16 mg_u16TxLinearDatFormatDiv128_VOUT(uint32 u32DataIn)
+static uint16 u16Ones(uint8 x)
 {
-  uint16 u16Result = 0;
-  CLRBIT(u16Result, 15); //SETBIT(u16Result,15);
-  uint8 u8Int = 0;
-  uint8 u8Fra = 0;
-
-  u8Int = u32DataIn >> 7;
-  u8Fra = (u32DataIn - u8Int * 128) * VOUT_FRC_RESULUTION >> 7;
-
-  u16Result += (u8Int << VOUT_FRC_NUM) + u8Fra;
-}
-*/
-
-static uint16 mg_u16TxLinearDatFormatDiv128_PSMI(uint32 u32DataIn, uint8 u16FrcNum, uint16 u16FrcRes)
-{
-  uint16 u16Result = 0;
-  uint16 u16Int = 0;
-  uint16 u16Fra = 0;
-
-  CLRBIT(u16Result, 15);
-  u16Int = (u32DataIn >> 7);
-  u16Fra = ((u32DataIn - u16Int * 128) * u16FrcRes >> 7);
-  u16Result += (u16Int << u16FrcNum) + u16Fra;
-
-  return u16Result;
+  uint16 u16Res;
+  u16Res = 0;
+  while(x--)
+  {    u16Res += 1<<x;  }
+  return u16Res;
 }
 
-// static uint16 mg_u16TxLinearDatFormatDiv128_PSMI(sint32 s32DataIn, uint8 u16FrcNum, uint16 u16FrcRes)
+// static uint16 mg_u16TxLinearDatFormatDiv128_PSMI(uint32 u32DataIn, uint8 u16FrcNum, uint16 u16FrcRes)
 // {
 //   uint16 u16Result = 0;
 //   uint16 u16Int = 0;
 //   uint16 u16Fra = 0;
 
-// 	if (s32DataIn >= 0)
-// 	{
-// 		CLRBIT(u16Result, 15);
-// 	}
-// 	else
-// 	{
-// 		SETBIT(u16Result, 15);
-// 	}
-
+//   CLRBIT(u16Result, 15);
 //   u16Int = (u32DataIn >> 7);
 //   u16Fra = ((u32DataIn - u16Int * 128) * u16FrcRes >> 7);
 //   u16Result += (u16Int << u16FrcNum) + u16Fra;
 
 //   return u16Result;
 // }
+
+static uint16 mg_u16TxLinearDatFormatDiv128_PSMI(sint32 s32DataIn, uint8 u16FrcNum, uint16 u16FrcRes)
+{
+  uint16 u16Result = 0;
+  uint16 u16Int = 0;
+  uint16 u16Fra = 0;
+  uint16 u16tmp1,u16tmp2;
+
+  u16tmp1 = u16Ones(16 - u16FrcNum);
+  u16tmp2 = u16Ones(u16FrcNum);
+
+	if (s32DataIn >= 0)
+	{
+    u16Int = (s32DataIn >> 7)& u16tmp1;
+    u16Fra = ((s32DataIn - u16Int * 128) * u16FrcRes >> 7) & u16tmp2;
+	}
+	else
+	{
+    s32DataIn = (~s32DataIn)+1;
+    u16Int = (s32DataIn >> 7)& u16tmp1;// 2^(16-1-6)-1
+    u16Fra = ((s32DataIn - u16Int * 128) * u16FrcRes >> 7) & u16tmp2;
+    u16Int = (~u16Int)+1;
+	}
+
+	u16Result += ((u16Int & u16tmp1) << u16FrcNum) + u16Fra;
+
+
+  return u16Result;
+}
 
 static uint16 mg_u16TxLinearDatFormatDiv128_PSMI_TEMP(sint16 s16DataIn)
 {
@@ -117,16 +118,18 @@ static uint16 mg_u16TxLinearDatFormatDiv128_PSMI_TEMP(sint16 s16DataIn)
 
 	if (s16DataIn >= 0)
 	{
-		CLRBIT(u16Result, 15);
+    u16Int = (s16DataIn >> 7)& 0x03FF;// 2^(16-1-6)-1
+    u16Fra = ((s16DataIn - u16Int * 128) * TEMPERATURE_FRC_RESULUTION >> 7) & 0x3F;
 	}
 	else
 	{
-		SETBIT(u16Result, 15);
+    s16DataIn = (~s16DataIn)+1;
+    u16Int = (s16DataIn >> 7)& 0x03FF;// 2^(16-1-6)-1
+    u16Fra = ((s16DataIn - u16Int * 128) * TEMPERATURE_FRC_RESULUTION >> 7) & 0x3F;
+    u16Int = (~u16Int)+1;
 	}
-	u16Int = (s16DataIn >> 7)& 0x01FF;// 2^(16-1-6)-1
-	u16Fra = ((s16DataIn - u16Int * 128) * TEMPERATURE_FRC_RESULUTION >> 7) & 0x3F;
-	u16Result += (u16Int << TEMPERATURE_FRC_NUM) + u16Fra;
 
+	u16Result += ((u16Int & 0x03FF) << TEMPERATURE_FRC_NUM) + u16Fra;
 	return u16Result;
 }
 
@@ -136,51 +139,69 @@ static float PsmiData_to_float(uint16 u16data, uint8 u16FrcNum, uint16 u16FrcRes
   float FInt, FFra, FRes;
   uint16 u16Int, u16Fra;
 
-  u16Int = ((u16data & 0x7FC0) >> u16FrcNum);
-  u16Fra = (u16data & 0x003F);
-  FFra = u16Fra / u16FrcRes;
+  u16Int = (u16data >> u16FrcNum)& (u16Ones(16-u16FrcNum));
+  u16Fra = (u16data & (u16Ones(u16FrcNum)));
+  FFra = (float)u16Fra / u16FrcRes;
 
   if(u16data & 0x8000)
   {
     //neg
-    u16Int = ((~u16Int) + 1) & 0x01FF;
+    u16Int = ((~u16Int) + 1) & (u16Ones(16-u16FrcNum));
     FInt = ((float)u16Int) * (-1);
+    FFra = FFra * (-1);
   }
   else
   {
     //pos
     FInt = u16Int;
   }
-  
   FRes = FInt + FFra;
-  
   return FRes;
 }
 
 #define AC_IIN_FORMAT(x)        mg_u16TxLinearDatFormatDiv128_PSMI(x,IIN_FRC_NUM,IIN_FRC_RESULUTION)
+
+
 
 int main(void)
 {
   uint16 a, b, c;
   float ff,f2;
   uint16 vout,iout,pout,vin,iin,pin,fanspeed;
-  ff = -5*128;
+  ff = -1.5*128;
 
-  c = mg_u16TxLinearDatFormatDiv128_PSMI_TEMP((sint16)ff);
-  f2 = PsmiData_to_float(c,TEMPERATURE_FRC_NUM,TEMPERATURE_FRC_RESULUTION);
+//  c = mg_u16TxLinearDatFormatDiv128_PSMI_TEMP((sint16)ff);
+//  f2 = PsmiData_to_float(c,TEMPERATURE_FRC_NUM,TEMPERATURE_FRC_RESULUTION);
+
+  // c = mg_u16TxLinearDatFormatDiv128_PSMI(3.4*128, VOUT_FRC_NUM, VOUT_FRC_RESULUTION);
+  // f2 = PsmiData_to_float(c,VOUT_FRC_NUM,VOUT_FRC_RESULUTION);
+  
+  // c = mg_u16TxLinearDatFormatDiv128_PSMI(-230*128, VIN_FRC_NUM, VIN_FRC_RESULUTION);
+  // f2 = PsmiData_to_float(c,VIN_FRC_NUM,VIN_FRC_RESULUTION);
+
+  // c = mg_u16TxLinearDatFormatDiv128_PSMI(-3.3*128, IOUT_FRC_NUM, IOUT_FRC_RESULUTION);
+  // f2 = PsmiData_to_float(c,IOUT_FRC_NUM,IOUT_FRC_RESULUTION);
+
+  // c = mg_u16TxLinearDatFormatDiv128_PSMI(-3.3*128, IIN_FRC_NUM, IIN_FRC_RESULUTION);
+  // f2 = PsmiData_to_float(c,IIN_FRC_NUM,IIN_FRC_RESULUTION);
+
+// c = mg_u16TxLinearDatFormatDiv128_PSMI(-3.6*128, POUT_FRC_NUM, POUT_FRC_RESULUTION);
+// f2 = PsmiData_to_float(c,POUT_FRC_NUM,POUT_FRC_RESULUTION);
+
+  // c = mg_u16TxLinearDatFormatDiv128_PSMI(-3.3*128, PIN_FRC_NUM, PIN_FRC_RESULUTION);
+  // f2 = PsmiData_to_float(c,PIN_FRC_NUM,PIN_FRC_RESULUTION);
+
+  c = mg_u16TxLinearDatFormatDiv128_PSMI(6500*128, FAN_SPEED_FRC_NUM, FAN_SPEED_FRC_RESULUTION);
+  f2 = PsmiData_to_float(c,FAN_SPEED_FRC_NUM,FAN_SPEED_FRC_RESULUTION);
+
   printf("%x,%f",c,f2);
   
-  // vout = mg_u16TxLinearDatFormatDiv128_PSMI(12*128, VOUT_FRC_NUM, VOUT_FRC_RESULUTION);
-  // iout = mg_u16TxLinearDatFormatDiv128_PSMI(1*128, IOUT_FRC_NUM, IOUT_FRC_RESULUTION);
+
+
   // pout = mg_u16TxLinearDatFormatDiv128_PSMI(ff*128, POUT_FRC_NUM, POUT_FRC_RESULUTION);
-  // vin = mg_u16TxLinearDatFormatDiv128_PSMI(230*128, VIN_FRC_NUM, VIN_FRC_RESULUTION);
-  // iin = mg_u16TxLinearDatFormatDiv128_PSMI(ff*128, IIN_FRC_NUM, IIN_FRC_RESULUTION);
   // pin = mg_u16TxLinearDatFormatDiv128_PSMI(ff*128, PIN_FRC_NUM, PIN_FRC_RESULUTION);
   // fanspeed = mg_u16TxLinearDatFormatDiv128_PSMI(6500*128, FAN_SPEED_FRC_NUM, FAN_SPEED_FRC_RESULUTION);
-  // printf("vout=%04x,\niout=%04x,\npout=%04x,\nvin=%04x,\niin=%04x,\npin=%04x,\nfanspeed=%04x\n", vout,iout,pout,vin,iin,pin,fanspeed);
-
   // vin = mg_u16TxLinearDatFormatDiv128_PSMI(279*128,VIN_FRC_NUM, VIN_FRC_RESULUTION);
-  // printf("%x",vin);
 
 
   return 0;
